54ac9096c12c92fcc2102d05e504a267bf41f47a
Mina Tahmasebi <mina_t_b@yahoo.com>
2020-09-24 04:18:36 +0200

    learn db code first

---
+++ Seminar.Database/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Seminar.Model;
+using System;
+
+namespace Seminar.Database
+{
+    public class SeminarDbContext : DbContext
+    {
+        public SeminarDbContext()
+        {
+
+        }
+
+        public DbSet<Dogh> Doghs { get; set; }
+    }
+}
